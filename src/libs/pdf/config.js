ce56e66b3b17d88984b3fe79e301b056f5e8b2db
muuuuminn <hiro.041310402.kt@gmail.com>
2024-10-20 23:11:34 +0900

    fix: update resume format

--- src/libs/pdf/config.js
+++ src/libs/pdf/config.js
@@ -18,7 +18,7 @@ module.exports = {
       </style>
       <section>
         <span>職務経歴書</span>
-        <span>2024/09/24</span>
+        <span>2024/10/18</span>
       </section>`,
     footerTemplate: `
       <div style="font-size: 10px; margin: 0 auto;">
