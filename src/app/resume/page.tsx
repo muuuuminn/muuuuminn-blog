2525f361e538892376a61437fb86e85e61574e06
muuuuminn <hiro.041310402.kt@gmail.com>
2024-11-14 02:01:43 +0900

    feat: replace resume for app router

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,34 @@
+import markdownToHtml from "zenn-markdown-html";
+
+import { RichMarkdownContent } from "@/shared/components/RichMarkdownContent";
+import { getMarkdownFileByFilename } from "@/libs/markdown/api";
+import { getDictionary } from "@/libs/i18n";
+import { getMetadata } from "@/libs/metadata";
+
+export async function generateMetadata() {
+  const d = await getDictionary();
+  const metadata = getMetadata({
+    title: d.PAGE.RESUME,
+    description: d.DESCRIPTION.RESUME,
+    path: "/resume",
+  });
+
+  return metadata;
+}
+
+const ResumePage = () => {
+  const resume = getMarkdownFileByFilename(
+    "resume",
+    ["title", "date", "slug", "content"],
+    "src/app/resume",
+  );
+  const content = markdownToHtml(resume.content);
+
+  return (
+    <div>
+      <RichMarkdownContent html={content} />
+    </div>
+  );
+};
+
+export default ResumePage;
