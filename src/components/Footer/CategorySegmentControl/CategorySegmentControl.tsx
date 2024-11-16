6355d4297f80b24b1112d9ae84d30e60559062b4
muuuuminn <hiro.041310402.kt@gmail.com>
2024-11-17 03:35:47 +0900

    fix: show category segment control in posts page

--- src/components/Footer/CategorySegmentControl/CategorySegmentControl.tsx
+++ src/components/Footer/CategorySegmentControl/CategorySegmentControl.tsx
@@ -6,23 +6,27 @@ import { CustomNextLink } from "@/libs/next/CustomNextLink";
 import { MASTER_CATEGORIES } from "@/features/category/constants";
 import { CategoryType } from "@/features/category/types";
 import styles from "./CategorySegmentControl.module.css";
-import { useParams } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
+
+const getHref = (category: CategoryType) => {
+  if (category.id === "-1") {
+    return `/posts`;
+  } else {
+    return `/posts/${category.name.toLowerCase()}`;
+  }
+};
 
 export const CategorySegmentControl = () => {
+  const pathname = usePathname();
   const { category_name } = useParams<{ category_name: string | undefined }>();
 
   const categories: CategoryType[] = [{ id: "-1", name: "All", color: "" }, ...MASTER_CATEGORIES];
 
-  const getHref = (category: CategoryType) => {
-    if (category.id === "-1") {
-      return `/posts`;
-    } else {
-      return `/posts/${category.name.toLowerCase()}`;
-    }
-  };
+  const hereIsPostsPage = pathname.includes("posts");
 
   return (
     <Flex
+      display={hereIsPostsPage ? "flex" : "none"}
       gap={{ initial: "2", sm: "1" }}
       align="center"
       justify="center"
